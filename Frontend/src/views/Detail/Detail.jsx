db84676bb5fad4a0542111d6370db5b87a16e8bc
sernapereira <serna.7a@gmail.com>
2023-04-27 19:26:06 -0500

    Estilos y logica en detail

--- Frontend/src/views/Detail/Detail.jsx
+++ Frontend/src/views/Detail/Detail.jsx
@@ -1,28 +1,76 @@
 import { Link, useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import style from "./Detail.module.css";
 import { getDogByIdAction } from "../../redux/Actions/dogActions";
 
 const Detail = () => {
+  const [position, setPosition] = useState(0);
+
   const { id } = useParams();
   const dispatch = useDispatch();
 
+  const dogDetail = useSelector((state) => state.dogs.dogDetail);
+
+  const images = dogDetail.imagen;
+  console.log(images);
+
+  const handlerPrev = () => {
+    setPosition(position === 0 ? images.length - 1 : position - 1);
+  };
+
+  const handleNext = () => {
+    setPosition(position === images.length - 1 ? 0 : position + 1);
+  };
+
+  //////////////////////////////////////////////
   useEffect(() => {
     dispatch(getDogByIdAction(id));
   }, []);
 
-  const dogDetail = useSelector((state) => state.dogs.dogDetail);
-  console.log(dogDetail?.imagen);
+  const displayedImages = images?.slice(position, position + 1);
 
   return (
-    <div className={style.detail}>
-      <figure className={style.detail__figure}>
-        {dogDetail.imagen?.map((dog, index) => {
-          return <img src={dog.image} alt="" className={style.detail__img} />;
-        })}
-      </figure>
+    <div className={style.container}>
+      <div className={style.detail}>
+        <div className={style.detail__listaImg}>
+          <button
+            onClick={(e) => handlerPrev(e)}
+            className={style.detail__prev}
+          >
+            <img
+              src="../../../public/avance-rapido.png"
+              alt=""
+              className={style.detail__flecha_prev}
+            />
+          </button>
+          <div className={style.detail__figure}>
+            {displayedImages?.map((dog, index) => {
+              return (
+                <div key={index} className={style.detail__figure_img}>
+                  <img
+                    src={dog.image}
+                    alt=""
+                    className={style.detail__img}
+                    key={index}
+                  />
+                </div>
+              );
+            })}
+          </div>
+          <button onClick={(e) => handleNext(e)} className={style.detail__prev}>
+            <img
+              src="../../../public/avance-rapido.png"
+              alt=""
+              className={style.detail__flecha_next}
+            />
+          </button>
+        </div>
 
+        <div className={style.detail__texts}>
+          <h2>Hola Soy {dogDetail.name}</h2>
+        </div>
+      </div>
     </div>
   );
 };
