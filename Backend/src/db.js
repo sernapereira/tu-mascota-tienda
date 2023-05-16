482f8fd2a5c52ece8961753d2c6e4567b06e2a02
sernapereira <serna.7a@gmail.com>
2023-05-16 15:13:49 -0500

    refactorización

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -3,10 +3,10 @@ const fs = require("fs");
 const path = require("path");
 const { Sequelize, Op } = require("sequelize");
 
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/mascotatienda`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
   {
     logging: false,
     native: false,
