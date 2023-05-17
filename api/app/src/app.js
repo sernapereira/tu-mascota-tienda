185c2ab2a57d7a416d4ff030a6785a4e11dca7a6
sernapereira <serna.7a@gmail.com>
2023-05-17 09:49:05 -0500

    cambios de acceso en api

--- api/app/src/app.js
+++ api/app/src/app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "https://tu-mascota-tienda.vercel.app");
   //   res.header(
   //     "Access-Control-Allow-Origin",
   //     "https://proyecto-final-henry-nu.vercel.app"
